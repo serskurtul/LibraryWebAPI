721e69f6c4c617d3f31d97487221b06b4d6ab35f
Serhii <ser.skurt@gmail.com>
2023-02-23 18:24:51 +0200

    Update AutomapperInitializer.cs

--- LibraryWebAPI/Configurations/AutomapperInitializer.cs
+++ LibraryWebAPI/Configurations/AutomapperInitializer.cs
@@ -5,19 +5,19 @@ using LibraryWebAPI.Data.ModelsDTO;
 
 namespace LibraryWebAPI.Configurations
 {
-	public class AutomapperInitilizer : Profile
-	{
-		public AutomapperInitilizer()
-		{
-			CreateMap<Book, BookDTO>()
-				.ForMember(x=>x.ReviewsNumber, opt=>opt.MapFrom(x=>x.Reviews.Count()))
-				.ForMember(x=>x.Rating, opt=>opt.MapFrom(x=>x.Rating.Score));
+    public class AutomapperInitilizer : Profile
+    {
+        public AutomapperInitilizer()
+        {
+            CreateMap<Book, BookDTO>()
+                .ForMember(x => x.ReviewsNumber, opt => opt.MapFrom(x => x.Reviews != null ? x.Reviews.Count : 0))
+                .ForMember(x => x.Rating, opt => opt.MapFrom(x => x.Ratings != null && x.Ratings.Count != 0 ? x.Ratings.Average(x => x.Score) : 0m));
             CreateMap<Review, ReviewDTO>().ReverseMap();
-			CreateMap<Book, BookReviewDetailsDTO>()
-				.ForMember(x => x.Rating, opt => opt.MapFrom(x => x.Rating.Score));
-			CreateMap<UpdateBookDTO, Book>();
-			CreateMap<CreateReviewDTO, Review>();
-			CreateMap<CreateRatingDTO, Rating>();
+            CreateMap<Book, BookReviewDetailsDTO>()
+                .ForMember(x => x.Rating, opt => opt.MapFrom(x => x.Ratings != null && x.Ratings.Count != 0 ? x.Ratings.Average(x => x.Score) : 0m));
+            CreateMap<UpdateBookDTO, Book>();
+            CreateMap<CreateReviewDTO, Review>();
+            CreateMap<CreateRatingDTO, Rating>();
         }
     }
 }
