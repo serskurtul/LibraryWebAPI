b1c19ae6011651a68cc16c06c4db2e7e83e880d3
Serhii <ser.skurt@gmail.com>
2023-02-23 18:27:02 +0200

    Context updated

--- LibraryWebAPI/Data/Models/Book.cs
+++ LibraryWebAPI/Data/Models/Book.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LibraryWebAPI.Data.Models
 {
@@ -13,12 +12,10 @@ namespace LibraryWebAPI.Data.Models
         public string Content { get; set; } = string.Empty;
         public string Author { get; set; } = string.Empty;
         public string Genre { get; set; } = string.Empty;
-        [ForeignKey(nameof(Rating))]
-        public int RatingId { get; set; }
 
         public virtual ICollection<Review> Reviews { get; set; }
 
-        public virtual Rating Rating { get; set; }
+        public virtual ICollection<Rating> Ratings { get; set; }
     }
 }
 
